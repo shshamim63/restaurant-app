f5cb9b988c2014a9f64ba0e088ceef91cafcb561
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-10-07 20:17:30 +0600

    trailing space removed

--- src/js/index.js
+++ src/js/index.js
@@ -8,15 +8,15 @@ aboutSection();
 
 const resetMenuSelection = () => {
   document.querySelectorAll('.menu-item').forEach((menuItem) =>	{
-		menuItem.classList.remove('activeheader')
-		});
+		menuItem.classList.remove('activeheader');
+	});
 };
 
-document.querySelectorAll('.menu-item').forEach( list =>
+document.querySelectorAll('.menu-item').forEach((list) => 
 	list.addEventListener('click', (e) => {
 		resetMenuSelection();
 		document.querySelector('.content').removeChild(document.querySelector('.container'));
-		switch(e.target.id){
+		switch(e.target.id) {
 			case 'about':
 				aboutSection();
 				break;
