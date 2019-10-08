ebd6270389d11c215f76b99d332e12802fa5b38b
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-10-08 15:41:15 +0600

    done with project

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const merge = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const common = require('./webpack.config');
 
 module.exports = merge(common, {
@@ -10,6 +11,15 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  }
 });
